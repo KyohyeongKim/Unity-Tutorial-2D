4cbad1da7e2811c87448ce18ff03ae3af2bd7c5a
김교형 <yd97531@gmail.com>
2025-05-21 16:31:55 +0900

    [ADD] New Script

---
+++ Assets/02. Scripts/StudyComponent.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class StudyComponent : MonoBehaviour
+{
+    GameObject obj; // 큐브 게임오브젝트를 가져오고 싶다.
+    public string changeName;
+
+    private void Start()
+    {
+        obj = GameObject.Find("Main Camera");
+        
+        obj.name = changeName;
+    }
+}
