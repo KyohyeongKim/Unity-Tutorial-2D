bf299cf0244e1766be6e833b63973bc8a0fc5884
김교형 <yd97531@gmail.com>
2025-05-29 23:32:09 +0900

    Update StudyMaterial.cs

--- Assets/02. Scripts/Study/StudyMaterial.cs
+++ Assets/02. Scripts/Study/StudyMaterial.cs
@@ -3,48 +3,29 @@ using UnityEngine;
 public class StudyMaterial : MonoBehaviour
 {
     public Material mat;
-
     public string hexCode;
 
     void Start()
     {
-        // this.GetComponent<Material>() = mat; // Material을 바꾸는 방식 X
+        // this.GetComponent<Material>() = mat; // Material을 이런 식으로는 바꿀 수 없다. (오류)
         
-        // this.GetComponent<MeshRenderer>().sharedMaterial = mat; // MeshRenderer에 접근해서 바꾸는 방식
+        this.GetComponent<MeshRenderer>().material = mat; // MeshRenderer에 접근해서 바꾸는 방식
+        this.GetComponent<MeshRenderer>().sharedMaterial = mat; // MeshRenderer에 접근해서 바꾸는 방식
         
-        // this.GetComponent<MeshRenderer>().material.color = Color.green;
+        this.GetComponent<MeshRenderer>().material.color = Color.green;
+        // 매터리얼 원본의 색상을 바꾼다. 따라서 동일한 매터리얼을 사용하는 모든 오브젝트의 색상이 바뀐다.
+        this.GetComponent<MeshRenderer>().sharedMaterial.color = Color.green; 
         
-        // this.GetComponent<MeshRenderer>().sharedMaterial.color = Color.green;
-        
+        // 수치값으로 색상 지정
         this.GetComponent<MeshRenderer>().material.color = new Color(200f/255f, 137f-255f, 70f-255f, 1);
         
+        // 헥스코드값으로 색상 지정
         mat = this.gameObject.GetComponent<MeshRenderer>().material;
         Color outputColor;
 
         if (ColorUtility.TryParseHtmlString(hexCode, out outputColor))
         {
             mat.color = outputColor;
-        }
-    }
-
-    // 대각선 이동이 불가능하므로 잘못된 조건
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            // 앞으로 가는 기능
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            // 왼쪽으로 가는 기능
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            // 뒤로 가는 기능
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            // 오른쪽으로 가는 기능
         }
     }
 }
